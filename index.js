5d618adee963c4348ff3fc7be885d0452c01cff5
Aman241999 <amansinghania453>
2023-08-11 14:20:25 +0530

    req-res

---
+++ index.js
@@ -0,0 +1,22 @@
+const http = require("http");
+
+const server = http.createServer((req, res) => {
+  if (req.url === "/home") {
+    res.writeHead(200, { "Content-Type": "text/plain" });
+    res.end("Welcome home");
+  } else if (req.url === "/about") {
+    res.writeHead(200, { "Content-Type": "text/plain" });
+    res.end("Welcome to About Us page");
+  } else if (req.url === "/node") {
+    res.writeHead(200, { "Content-Type": "text/plain" });
+    res.end("Welcome to my Node Js project");
+  } else {
+    res.writeHead(404, { "Content-Type": "text/plain" });
+    res.end("404 - Not Found");
+  }
+});
+
+const port = 3000;
+server.listen(port, () => {
+  console.log(`Server is listening on port ${port}`);
+});
